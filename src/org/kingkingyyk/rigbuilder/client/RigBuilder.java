62b8c545ddb036ee6ae1e1f4f30ff89922a58e1b
kingkingyyk <king8793yyk@hotmail.com>
2016-08-26 17:03:32 +0800

    Dark Theme Update
    - Added fading background
    - Added site update information and GitHub link

--- src/org/kingkingyyk/rigbuilder/client/RigBuilder.java
+++ src/org/kingkingyyk/rigbuilder/client/RigBuilder.java
@@ -7,8 +7,11 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ChangeEvent;
 import com.google.gwt.event.dom.client.ChangeHandler;
+import com.google.gwt.user.client.Random;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ListBox;
@@ -50,47 +53,86 @@ public class RigBuilder implements EntryPoint {
 		}
 		
 		public boolean hasMoreTokens() {
-			return currIndex<=sAry.length;
+			return currIndex<sAry.length;
 		}
 	}
 	
 	private ArrayList<String> list=new ArrayList<>();
 	
 	public void onModuleLoad() {
+		RootPanel.getBodyElement().getStyle().setBackgroundColor("black");
+		
 		final HorizontalPanel recomHP=new HorizontalPanel();
 		final Label recomText=new Label("Recommended Builds :  ");
 		final ListBox recomlb=new ListBox();
+		recomText.getElement().getStyle().setProperty("color", "white");
+		recomlb.getElement().getStyle().setBackgroundColor("rgba(0,0,0,0.6)");
+		recomlb.getElement().getStyle().setColor("white");
 		recomHP.add(recomText);
 		recomHP.add(recomlb);
 		RootPanel.get("recommended").add(recomHP);
 		
-		RootPanel.get("seperator").add(new Label("==========================================="));		
+		final Label sep=new Label("--------------------------------------------");
+		sep.getElement().getStyle().setProperty("color", "white");
+		RootPanel.get("seperator").add(sep);
 				
 		final HorizontalPanel hp=new HorizontalPanel();
-		final Label totalTxt=new Label("Total :    ");
+		final Label totalTxt=new Label("Total : RM ");
 		final Label totalValue=new Label("0");
 		hp.add(totalTxt);
 		hp.add(totalValue);
 		totalTxt.getElement().getStyle().setProperty("fontSize", "150%");
+		totalTxt.getElement().getStyle().setProperty("color", "white");
 		totalValue.getElement().getStyle().setProperty("color", "red");
 		totalValue.getElement().getStyle().setProperty("fontWeight", "bold");
 		totalValue.getElement().getStyle().setProperty("fontSize", "150%");
 		RootPanel.get("total").add(hp);
 		
-		RootPanel.get("disclaimer").add(new Label("Since there is no constraint in mix-and-match, it is important to check the compatiblity before buying!"));		
+		final Label disclaimer=new Label("Since there is no constraints in mix-and-match, it is important to check the compatiblity before buying!");
+		disclaimer.getElement().getStyle().setProperty("color", "white");
+		RootPanel.get("disclaimer").add(disclaimer);		
 		
-		final Label lastUpdate=new Label();
-		lastUpdate.setText("Loading");
+		final HTML lastUpdate=new HTML();
+		lastUpdate.setHTML("Loading");
+		lastUpdate.getElement().getStyle().setProperty("color", "white");
 		RootPanel.get("lastUpdate").add(lastUpdate);
 		
 		greetingService.greetServer("", new AsyncCallback<ArrayList<String>>() {
 			public void onFailure(Throwable caught) {
-				lastUpdate.setText("Fail");
+				lastUpdate.setHTML("Fail");
 			}
 
 			public void onSuccess(ArrayList<String> result) {
 				list=result;
-				lastUpdate.setText("Last Update : "+list.get(0)+". Powered by Google Web Toolkit 2.7.0");
+				lastUpdate.setHTML("Price Update : "+list.get(0)+" | Site Update : 26 Aug 2016<br/>Source Code available on <a href=\"https://github.com/kingkingyyk/RigBuilder\">GitHub</a> | Powered by Google Web Toolkit 2.7.0");
+				
+				//for background
+				final ArrayList<String> backgrounds=new ArrayList<>();
+				int backgroundIndex=0;
+				for (int i=0;i<list.size();i++) {
+					if (list.get(i).startsWith("BACKGROUND IMAGES")) {
+						backgroundIndex=i;
+						break;
+					}
+				}
+				StringTokenizer st=new StringTokenizer(list.get(backgroundIndex));
+				st.nextToken(); //remove the background image
+				while (st.hasMoreTokens()) {
+					backgrounds.add(st.nextToken());
+				}
+				RootPanel.getBodyElement().getStyle().setProperty("transition","background 1000ms ease-in 1000ms");
+				RootPanel.getBodyElement().getStyle().setBackgroundImage("url("+backgrounds.get(Random.nextInt(backgrounds.size()))+")");
+				Timer backgroundTimer=new Timer() {
+					private int id=Random.nextInt(backgrounds.size());
+					@Override
+					public void run() {
+						RootPanel.getBodyElement().getStyle().setBackgroundImage("url("+backgrounds.get(id)+")");
+						id=(id+1)%backgrounds.size();
+					}
+				};
+				backgroundTimer.scheduleRepeating(10000);
+
+				
 				//for builds.
 				int startIndex=0;
 				for (int i=0;i<list.size();i++) {
@@ -108,12 +150,11 @@ public class RigBuilder implements EntryPoint {
 				}
 				
 				ArrayList<String> componentList=new ArrayList<>();
-				StringTokenizer st=new StringTokenizer(list.get(startIndex));
+				st=new StringTokenizer(list.get(startIndex));
 				st.nextToken(); st.nextToken();
 				while (st.hasMoreTokens()) {
 					componentList.add(st.nextToken());
 				}
-				componentList.remove(componentList.size()-1); //remove empty.
 				
 				hardware=componentList.toArray(new String [componentList.size()]);
 				for (int i=0;i<hardware.length;i++) {
@@ -174,10 +215,14 @@ public class RigBuilder implements EntryPoint {
 				final Grid g=new Grid(hardware.length,3);
 				final ListBox [] lb=new ListBox [hardware.length];
 				for (int i=0;i<hardware.length;i++) {
-					g.setText(i,0,hardware[i]);
+					Label componentName=new Label(hardware[i]);
+					componentName.getElement().getStyle().setProperty("color", "white");
+					g.setWidget(i,0,componentName);
 					currComp=strToComp.get(hardware[i]);
 					lb[i]=new ListBox();
 					lb[i].getElement().getStyle().setWidth(350, Unit.PX);
+					lb[i].getElement().getStyle().setBackgroundColor("rgba(0,0,0,0.6)");
+					lb[i].getElement().getStyle().setColor("white");
 					for (Item it : currComp.itemList) {
 						lb[i].addItem(it.name);
 					}
@@ -186,7 +231,7 @@ public class RigBuilder implements EntryPoint {
 						@Override
 						public void onChange(ChangeEvent event) {
 							ListBox fired=(ListBox)event.getSource();
-							g.setText(boxPosition.get(fired),2,String.valueOf(strToItem.get(g.getText(boxPosition.get(fired),0)+fired.getSelectedItemText()).price));
+							((Label)g.getWidget(boxPosition.get(fired),2)).setText(String.valueOf(strToItem.get(g.getText(boxPosition.get(fired),0)+fired.getSelectedItemText()).price));
 							int sum=0;
 							for (int i=0;i<hardware.length;i++) {
 								sum+=Integer.parseInt(g.getText(i,2));
@@ -196,7 +241,9 @@ public class RigBuilder implements EntryPoint {
 						
 					});
 					g.setWidget(i,1, lb[i]);
-					g.setText(i,2,"0");
+					Label price=new Label("0");
+					price.getElement().getStyle().setProperty("color", "white");
+					g.setWidget(i,2,price);
 				}
 
 				RootPanel.get("spec").add(g);
@@ -213,7 +260,8 @@ public class RigBuilder implements EntryPoint {
 								}
 							}
 							
-							g.setText(i,2,String.valueOf(strToItem.get(g.getText(i,0)+lb[i].getSelectedItemText()).price));
+							((Label)g.getWidget(i,2)).setText(String.valueOf(strToItem.get(g.getText(i,0)+lb[i].getSelectedItemText()).price));
+							
 						}
 						int sum=0;
 						for (int i=0;i<hardware.length;i++) {
