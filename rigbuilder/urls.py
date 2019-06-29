ede4119ed5ed7b792cdedb3cd6e776d9409cafe2
King <king8793yyk@hotmail.com>
2019-06-29 18:42:16 +0800

    Added default view

--- rigbuilder/urls.py
+++ rigbuilder/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.home),
     path('rigbuilder/', include('rigbuildersite.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
